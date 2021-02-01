dd4af7cd385724aed8de7f0766247ab852e7c707
Tomasz B <tomaszbanach12@gmail.com>
2021-02-01 12:04:35 +0100

    End of chapter 2

---
+++ DemoCode.Tests/StringDemos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using AutoFixture;
+
+namespace DemoCode.Tests
+{
+    public class StringDemos
+    {
+        [Fact]
+        public void Strings()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var sut = new NameJoiner();
+
+            var firstName = fixture.Create<string>();
+            var lastName = fixture.Create<string>();
+
+            // Act
+            var result = sut.Join(firstName, lastName);
+
+            // Assert
+            Assert.True(result == $"{firstName} {lastName}");
+        }
+    }
+}
---
+++ DemoCode/NameJoiner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoCode
+{
+    public class NameJoiner
+    {
+        public int MyProperty { get; set; }
+
+        public string Join(string firstName, string lastName)
+        {
+            return $"{ firstName } { lastName }";
+        }
+    }
+}
